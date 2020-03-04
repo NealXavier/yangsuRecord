a010c6b63ce81ac4b9ad53386053650976e4ee18
NealXavier <nealxavier@163.com>
2020-03-04 23:23:44 +0800

    just quick sort

---
+++ Algorithm/leetcode/75_sortColors.js
@@ -0,0 +1,26 @@
+var sortColors = function(nums) {
+  // [2,0,2,1,1,0]
+  return quickSort(nums,0,nums.length- 1)
+};
+
+var quickSort = function(nums,left,right){
+  if(left >=right) return;
+  debugger
+  var pivot = participation(nums,left,right)
+  quickSort(nums,left,pivot)
+  quickSort(nums,pivot+1,right)
+  return nums
+}
+
+var participation = function(nums,L,R){
+  var i = L,j = R;
+  var pivot = nums[L]
+  while(i<j){
+    while(i<j && nums[j]>=pivot)j--
+    nums[i] = nums[j]
+    while(i<j && nums[i]<=pivot)i++
+    nums[j] = nums[i]
+  }
+  nums[i] = pivot
+  return i
+}
